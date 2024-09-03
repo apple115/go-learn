b5373a301fb8069a53ed878ddae6c1160d10781a
apple115 <2567031030@qq.com>
2024-09-03 10:53:34 +0800

    学习了一点io

--- learnGoWithTests/http-server/server_integration_test.go
+++ learnGoWithTests/http-server/server_integration_test.go
@@ -6,24 +6,13 @@ import (
 	"testing"
 )
 
-func TestRecordingWinAndRetrievingThem(t *testing.T) {
-	store := NewInMemoryPlayerStore()
-	server := NewPlayerServer(store)
-	player := "Pepper"
-
-	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
-	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
-	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
-
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
-	assertStatus(t, response.Code, http.StatusOK)
-	assertResponseBody(t, response.Body.String(), "3")
-
-}
-//server_integration_test.go
+// server_integration_test.go
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
-	store := NewInMemoryPlayerStore()
+	//server_integration_test.go
+		database, cleanDatabase := createTempFile(t,`[]`)
+	defer cleanDatabase()
+	store, err := NewFileSystemPlayerStore(database)
+	assertNoError(t, err)
 	server := NewPlayerServer(store)
 	player := "Pepper"
 
