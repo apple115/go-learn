e1c0d1301e12e4f6319ebe7dc89b0e52859a59b6
apple115 <2567031030@qq.com>
2024-09-08 22:18:47 +0800

    初步完成 但是有bug

--- gee-learn/gee/context.go
+++ gee-learn/gee/context.go
@@ -15,6 +15,14 @@ type Context struct {
 	Method     string
 	Params     map[string]string
 	StatusCode int
+	//middleware
+	handlers []HandlerFunc
+	index    int
+}
+
+func (c *Context) Fail(code int, err string) {
+	c.index = len(c.handlers)
+	c.JSON(code, H{"message": err})
 }
 
 // Param ...
@@ -29,6 +37,15 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 		Req:    req,
 		Path:   req.URL.Path,
 		Method: req.Method,
+		index:  -1,
+	}
+}
+
+func (c *Context) Next() {
+	c.index++
+	s := len(c.handlers)
+	for ; c.index < s; c.index++ {
+		c.handlers[c.index](c)
 	}
 }
 
