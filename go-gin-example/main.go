f4714505679f816ba587f57bf430ec785b20ddd8
apple115 <2567031030@qq.com>
2024-09-17 14:42:21 +0800

    实现了优雅的重启关闭

--- go-gin-example/main.go
+++ go-gin-example/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"go-gin-example/pkg/setting"
 	"go-gin-example/routers"
-	"net/http"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/fvbock/endless"
 )
 
 func main() {
-	router := routers.InitRouter()
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+	s := endless.NewServer(endPoint, routers.InitRouter())
+	s.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen:%s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("shutdwon server...")
+	ctx, concel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer concel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Sever exiting")
 }
